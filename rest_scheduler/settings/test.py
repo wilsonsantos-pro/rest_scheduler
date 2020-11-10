9af347c4718554da4265bad8e9e0ff32a0405f29
Wilson Santos <wilson.santos.dev87@gmail.com>
2020-11-10 17:47:34 +0100

    Added test settings and Travis CI config

---
+++ rest_scheduler/settings/test.py
@@ -0,0 +1,11 @@
+from .base import *
+from .local import SECRET_KEY
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
